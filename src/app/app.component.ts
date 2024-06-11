d39fee7fa340e164195ccfbbe64b387289638200
MaximKraychev <maxim.kraychev@gmail.com>
2024-06-11 15:34:21 +0300

    Add header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component.js';
+import { UserInputsComponent } from './user-inputs/user-inputs.component.js';
+import { InvestmentResultsComponent } from './investment-results/investment-results.component.js';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, UserInputsComponent, InvestmentResultsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
